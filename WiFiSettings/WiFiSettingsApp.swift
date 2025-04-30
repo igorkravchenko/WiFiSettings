94d0065ba877f101b125bf818e96d72c8f1bab0c
Igor Kravchenko <igman2005@gmail.com>
2025-04-30 18:43:26 +0300

    Initial Commit

---
+++ WiFiSettings/WiFiSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSettingsApp.swift
+//  WiFiSettings
+//
+//  Created by Kravchenko, Ihor on 30.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WiFiSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WiFiSettingsTests/WiFiSettingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSettingsTests.swift
+//  WiFiSettingsTests
+//
+//  Created by Kravchenko, Ihor on 30.04.2025.
+//
+
+import Testing
+@testable import WiFiSettings
+
+struct WiFiSettingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
